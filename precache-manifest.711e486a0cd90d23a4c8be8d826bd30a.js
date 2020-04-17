d8093c6191042b64e58102c1cffc6783fafea2ea
Rocky Liwanag <studiorockprod@me.com>
2020-04-17 16:48:44 -0700

    Updates

---
+++ precache-manifest.711e486a0cd90d23a4c8be8d826bd30a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc333e9aee236f7ba38e99aecbe5b6b2",
+    "url": "/rockyliwanag/proj4assessment/index.html"
+  },
+  {
+    "revision": "3988bfa53bd737e3d321",
+    "url": "/rockyliwanag/proj4assessment/static/css/main.64e51c26.chunk.css"
+  },
+  {
+    "revision": "bf9faed5984ec60f7781",
+    "url": "/rockyliwanag/proj4assessment/static/js/2.36c0ca1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rockyliwanag/proj4assessment/static/js/2.36c0ca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3988bfa53bd737e3d321",
+    "url": "/rockyliwanag/proj4assessment/static/js/main.ec9c2151.chunk.js"
+  },
+  {
+    "revision": "e0ac89bfcd42100d40eb",
+    "url": "/rockyliwanag/proj4assessment/static/js/runtime-main.5c0477b2.js"
+  }
+]);
